c250a96529b85f291d31e947d7e471af43913feb
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2025-10-13 22:11:44 +0530

    Update RegSpicesPage.js

--- frontend/src/components/AfterRegistered/RegSpicesPage/RegSpicesPage.js
+++ frontend/src/components/AfterRegistered/RegSpicesPage/RegSpicesPage.js
@@ -11,7 +11,7 @@ function SpicesPage() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch(process.env.REACT_APP_API_URL/product/Spices');
+        const response = await fetch(`process.env.REACT_APP_API_URL/product/Spices`);
         const data = await response.json();
         setProducts(data);
       } catch (error) {
